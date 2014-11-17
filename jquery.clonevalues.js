f4d28181dad60518f7744b2d3ed82de0d48ccb73
Stephan Langenegger <sl@stibo.ch>
2014-11-17 21:36:17 +0100

    Bugs fixed and textarea added

--- jquery.clonevalues.js
+++ jquery.clonevalues.js
@@ -1,4 +1,4 @@
-/*! jQuery clone with form values v1.0.0 | (c) 2014 FAKTOR VIER GmbH | http://faktorvier.ch */
+/*! jQuery clone with form values v1.0.1 | (c) 2014 FAKTOR VIER GmbH | http://faktorvier.ch */
 
 (function($) {
 	$.fn.cloneValues = function(withDataAndEvents, deepWithDataAndEvents) {
@@ -6,22 +6,24 @@
 		withDataAndEvents = withDataAndEvents || false;
 		deepWithDataAndEvents = deepWithDataAndEvents || withDataAndEvents;
 
-		// Clone object
-		var $clonedObject = this.clone(withDataAndEvents, deepWithDataAndEvents);
-
 		// Set and check input/textarea values
-		$clonedObject.find('input, textarea').each(function() {
+		this.find('input, textarea').each(function() {
 			if($(this).is(find('input[type="checkbox"], input[type="radio"]'))) {
-				$(this).prop('checked', $(this).prop('checked'));
+				$(this).attr('checked', $(this).prop('checked'));
+			} else if($(this).get(0).tagName.toLowerCase() == 'textarea') {
+				$(this).html($(this).val());
 			} else {
 				$(this).attr('value', $(this).val());
 			}
 		});
 
 		// Select selected options
-		$clonedObject.find('select option').each(function() {
-			$(this).prop('selected', $(this).prop('selected'));
+		this.find('select option').each(function() {
+			$(this).attr('selected', $(this).prop('selected'));
 		});
+
+		// Clone object
+		var $clonedObject = this.clone(withDataAndEvents, deepWithDataAndEvents);
 
 		return $clonedObject;
 	};
